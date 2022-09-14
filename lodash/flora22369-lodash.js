b8090fb6461df6b90b9675c125e08911a9d69eee
Flora <3303357207@qq.com>
2022-09-14 21:28:37 +0800

    1

--- lodash/flora22369-lodash.js
+++ lodash/flora22369-lodash.js
@@ -1,4 +1,18 @@
 var flora22369={
-    chunk:function(){},
+    chunk:function(array, size = 1){
+        var rest = []
+        var temp = []
+        for(var i=0 ; i<array.length ; i++){
+            temp.push(array[i])
+            if(temp.length===size){
+                rest.push(temp)
+                temp = []
+            }
+        }
+        if(temp[0]){
+            rest.push(temp)
+        }
+        return rest
+    },
          
 }
